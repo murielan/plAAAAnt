9330531520c9c7ee456551d2e8b6c9fc0f162fbf
murielan <muriel.andermatt@students.fhnw.ch>
2023-11-06 16:29:29 +0100

    topbar plant

--- app/src/main/kotlin/fhnw/ws6c/theapp/ui/screens/PlantScreen.kt
+++ app/src/main/kotlin/fhnw/ws6c/theapp/ui/screens/PlantScreen.kt
@@ -5,19 +5,30 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import fhnw.ws6c.theapp.model.PlantModel
+import fhnw.ws6c.theapp.model.Screen
 
 @Composable
 fun PlantScreen(model: PlantModel) {
     with(model) {
         Scaffold(
+            topBar = { PlantAppBar(model) },
             content = { innerPadding -> SinglePlantContent(model, innerPadding) },
             bottomBar = { NavigationBottomAppBar(model) }
         )
@@ -46,3 +57,32 @@ fun SinglePlantContent(model: PlantModel, innerPadding: PaddingValues) {
         }
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PlantAppBar(model: PlantModel) {
+    with(model) {
+        CenterAlignedTopAppBar(
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.primary,
+            ),
+            title = {
+                Text(
+                    currentPlant.name,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            },
+            navigationIcon = {
+                IconButton(onClick = { currentScreen = Screen.HOME }) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Localized description"
+                    )
+                }
+            }
+        )
+    }
+}
+
